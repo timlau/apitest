9c2210b95f848e2a83c77f56e57a4434407ae34f
Tim Lauridsen <tim.lauridsen@gmail.com>
2024-06-12 19:12:09 +0200

    flatpak: get number of updates

---
+++ flatpak/get-updates.py
@@ -0,0 +1,9 @@
+import gi
+gi.require_version('Flatpak', '1.0')
+from gi.repository import Flatpak # type: ignore  # noqa: E402
+
+user_upd = len(Flatpak.Installation.new_user().list_installed_refs_for_update())
+system_upd = len(Flatpak.Installation.new_system().list_installed_refs_for_update())
+
+print(f"User updates : {user_upd}")
+print(f"System updates : {system_upd}")
