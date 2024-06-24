8306ca9d4c088ea1482e8e2e0c526a330c1185a6
Tim Lauridsen <tla@rasmil.dk>
2024-06-24 16:52:41 +0200

    dnf5: add memory leak test

---
+++ dnf5/mem_updates.py
@@ -0,0 +1,52 @@
+# Test dnf metadata mem-leak
+#
+# run with:
+#
+# /bin/python -m memray run --live ./dnf5/mem_updates.py
+
+
+
+import time
+import subprocess
+import shlex
+
+import libdnf5 as dnf
+
+
+def check_updates():
+    base = dnf.base.Base()
+    try:
+        # Setup dnf base
+        cache_directory = base.get_config().get_cachedir_option().get_value()
+        base.get_config().get_system_cachedir_option().set(cache_directory)
+        base.load_config()
+        base.setup()
+        # Setup repositories
+        base.repo_sack = base.get_repo_sack()
+        base.repo_sack.create_repos_from_system_configuration()
+        base.repo_sack.update_and_load_enabled_repos(True)
+        # Get availble updates
+        updates = dnf.rpm.PackageQuery(base)
+        updates.filter_upgrades()
+        updates.filter_arch(["src"], dnf.common.QueryCmp_NEQ)
+        updates.filter_latest_evr()
+        return len(list(updates))
+    finally:
+        del base
+
+
+def main():
+    expire_cmd=shlex.split("dnf5 clean expire-cache")
+    print(expire_cmd)
+    for x in range(3):
+        print(check_updates())
+        time.sleep(5)
+    for x in range(3):
+        subprocess.call(expire_cmd, shell=False)
+        print(check_updates())
+        time.sleep(5)
+    time.sleep(20)
+    
+
+if __name__ == "__main__":
+    main()
